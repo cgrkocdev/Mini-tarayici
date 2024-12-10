f47a4ab93b63eb315a98fa1b2980441177815e8b
Çağrı KOÇ <cagrikocank@gmail.com>
2024-12-10 22:02:26 +0300

    Mini-Tarayici

---
+++ app/src/main/java/com/example/minitarayc/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.minitarayc;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+EditText et1;
+WebView web1;
+Button btn1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        et1 = findViewById(R.id.editText1);
+        web1 = findViewById(R.id.webView1);
+        btn1 = findViewById(R.id.button1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              String myUrl =  et1.getText().toString();
+              web1.setWebViewClient(new WebViewClient());
+              web1.loadUrl(myUrl);
+            }
+        });
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
